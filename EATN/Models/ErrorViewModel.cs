a9eebde8deeebe441edebfe209c6b46b60c7b5e8
Partey, Manish <manish.partey@duckcreek.com>
2020-05-28 00:01:46 +0530

    First Commit

---
+++ EATN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EATN.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
